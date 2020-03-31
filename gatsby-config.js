1ed3645e148734061cf8dd93dce9f20494d4a0fa
Liam Silk <liamjosephsilk@gmail.com>
2020-03-31 13:22:24 +0100

    Added to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,28 +15,58 @@ module.exports = {
         name: `ComicBoxRumble`,
       },
     },
-    `gatsby-plugin-netlify-cms`,
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/static/assets`,
+        name: `blog-images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/content/blog`,
+      },
+    },
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-transformer-remark`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+            options: {
+              name: "blog-images",
+              maxWidth: 200,
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              name: "blog-images",
+              maxWidth: 200,
+            },
+          },
+        ],
       },
     },
+    `gatsby-plugin-netlify-cms`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,40 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const About = () => (
-  <Layout>
-    <SEO title="About" />
-    <h1>About</h1>
-    <p>Welcome to page 2</p>
-  </Layout>
-)
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query Blog {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            frontmatter {
+              title
+              description
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="About" />
+      <h1>About</h1>
+      <p>Welcome to page 2</p>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div>
+          <h1>{node.frontmatter.title}</h1>
+          <h2>{node.frontmatter.description}</h2>
+          <div dangerouslySetInnerHTML={{ __html: node.html }}></div>
+        </div>
+      ))}
+    </Layout>
+  )
+}
 
 export default About
