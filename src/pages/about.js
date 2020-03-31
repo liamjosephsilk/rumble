01bc8beef2d5cc0a38297e53117dab13766e05e3
Liam Silk <liamjosephsilk@gmail.com>
2020-03-31 15:04:48 +0100

    Debuggiing issues

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,34 +5,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const About = () => {
-  const data = useStaticQuery(graphql`
-    query Blog {
-      allMarkdownRemark {
-        edges {
-          node {
-            html
-            frontmatter {
-              title
-              description
-            }
-          }
-        }
-      }
-    }
-  `)
-
   return (
     <Layout>
       <SEO title="About" />
       <h1>About</h1>
       <p>Welcome to page 2</p>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <div>
-          <h1>{node.frontmatter.title}</h1>
-          <h2>{node.frontmatter.description}</h2>
-          <div dangerouslySetInnerHTML={{ __html: node.html }}></div>
-        </div>
-      ))}
     </Layout>
   )
 }
