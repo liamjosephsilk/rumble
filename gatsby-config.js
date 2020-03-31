d62323ff6edb1ccaa2d5db21a6b513792a7f8d8c
Liam Silk <liamjosephsilk@gmail.com>
2020-03-31 22:20:00 +0100

    config for images in frontmatter

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,12 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          `gatsby-remark-relative-images`,
+          {
+            resolve: `gatsby-remark-relative-images`,
+            options: {
+              name: "blog-images",
+            },
+          },
           {
             resolve: `gatsby-remark-images`,
             options: {
